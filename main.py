cef7b9fa8811892406210fb9274d8928c13f4b1a
DBerrones <53327546+danielberrones@users.noreply.github.com>
2020-01-01 19:32:06 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,204 @@
+class Warrior:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Animal:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Insect:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Planet:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Oceans:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Stars:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+	
+class Aliens:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Spaceships:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Weapons:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Points:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+class Rules:
+	pass
+	def __init__(self):
+		pass
+	def method1(self):
+		pass
+	def method2(self):
+		pass
+	def method3(self):
+		pass
+	def method1a(self):
+		pass
+	def method2b(self):
+		pass
+	def method3c(self):
+		pass
+
+
+def main():
+	pass
+
+
+if '__name__' == '__main__':
+	main()
